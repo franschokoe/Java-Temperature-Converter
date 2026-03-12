92fc11c7b06c272ae2ab943836bfc6497d8e416d
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-12 22:27:41 +0200

    Introduced the while lop and

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -8,7 +8,36 @@ public class TemperatureConverter {
         boolean programRunner = true;
 
         //initiallizing the while loop
+        while (programRunner){
+            int decisionOutput = JOptionPane.showConfirmDialog(
+                    null ,
+                    "Do you want to convert from F to C?\nYes to confirm, No to convert from C to F and Cancel to 'exit'" ,
+                    "Converter",
+                    JOptionPane.YES_NO_CANCEL_OPTION
+            );
+            int Yes = JOptionPane.YES_OPTION;
+            int No = JOptionPane.NO_OPTION;
 
+            //Selection
+            if(JOptionPane.YES_OPTION == Yes){
+                double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
+                        null,
+                        "Enter your temperature in Fahrenheit:"
+                        ));
+
+                System.out.println("Hello Yes" + userTemp);
+
+                programRunner = false;
+            }
+            else if (JOptionPane.NO_OPTION == No) {
+                double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
+                        null,
+                        "Enter your temperature in Celsius:"
+
+                ));
+                System.out.println("Hello No..");
+            }
+        }
 
     }
 }
