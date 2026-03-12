1a0455d6ad729be48fd3062a84d0355b7a861921
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-13 00:49:41 +0200

    ERRORS HANDLING 5

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -10,97 +10,94 @@ public class TemperatureConverter {
 
         //initiallizing the while loop
         while (programRunner) {
-//            int decisionOutput = 0;
-            int decisionOutput = 0;
-            try {
-                decisionOutput = JOptionPane.showConfirmDialog(
+            try{
+                int decisionOutput = JOptionPane.showConfirmDialog(
                         null,
                         "Do you want to convert from F to C?\n\nYes to confirm, No to convert from C to F " +
                                 "and Cancel to 'exit'",
                         "Converter",
                         JOptionPane.YES_NO_CANCEL_OPTION
                 );
-            } catch (NumberFormatException e) {
+                //Selection
+                if (JOptionPane.YES_OPTION == decisionOutput) {
+                    //formating the string to double
+                    double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
+                            null,
+                            "Enter temperature in Fahrenheit:"
+                    ));
+                    //managing error handling
+                    try {
+                        // converting to celsius and formarting the results to 2 decimal
+                        double showNewTemp = fahrenheit_to_celsius * (userTemp - 32);
+                        JOptionPane.showMessageDialog(
+                                null,
+                                String.format("The Temperature in Celsius is: %.2f C", userTemp),
+                                "Results",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                        //for debuging
+                        //System.out.println("Hello Yes " + showNewTemp);
+
+                        break; //breaking out of the while loop
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Please enter only numbers",
+                                "Errro",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                } else if (JOptionPane.NO_OPTION == decisionOutput) {
+                    //formating the string to double
+                    double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
+                            null,
+                            "Enter temperature in Celsius"
+                    ));
+                    try {
+                        //converting to Fahrenheit
+                        double showNewTemp = (celsius_to_fahrenheit * userTemp) + 32;
+                        JOptionPane.showMessageDialog(
+                                null,
+                                String.format("The Temperature in Celsius is: %.2f C", showNewTemp),
+                                "Results",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                        //debuging
+                        //System.out.println("Hello No " + showNewTemp);
+                        break;
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Please enter correct charecters",
+                                "Error",
+                                JOptionPane.ERROR_MESSAGE
+                        );
+                    }
+
+                } else if (JOptionPane.CANCEL_OPTION == decisionOutput) {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Thank for using our Temperatures Converter.\n\nBye",
+                            "Closing Program",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                    break;
+                } else {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Error Occured Please try again.",
+                            "404",
+                            JOptionPane.ERROR);
+                    break;
+                }
+
+            }catch(NumberFormatException e){
                 JOptionPane.showMessageDialog(
                         null,
                         "Invalid input please try again",
                         "Error",
-                        JOptionPane.ERROR
+                        JOptionPane.ERROR_MESSAGE
                 );
-            }
-
-            //Selection
-            if (JOptionPane.YES_OPTION == decisionOutput) {
-                //formating the string to double
-                double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
-                        null,
-                        "Enter temperature in Fahrenheit:"
-                ));
-                //managing error handling
-                try {
-                    // converting to celsius and formarting the results to 2 decimal
-                    double showNewTemp = fahrenheit_to_celsius * (userTemp - 32);
-                    JOptionPane.showMessageDialog(
-                            null,
-                            String.format("The Temperature in Celsius is: %.2f C", userTemp),
-                            "Results",
-                            JOptionPane.INFORMATION_MESSAGE
-                    );
-                    //for debuging
-                    //System.out.println("Hello Yes " + showNewTemp);
-
-                    break; //breaking out of the while loop
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Please enter only numbers",
-                            "Errro",
-                            JOptionPane.ERROR_MESSAGE);
-                }
-            } else if (JOptionPane.NO_OPTION == decisionOutput) {
-                //formating the string to double
-                double userTemp = Double.parseDouble(JOptionPane.showInputDialog(
-                        null,
-                        "Enter temperature in Celsius"
-                ));
-                try {
-                    //converting to Fahrenheit
-                    double showNewTemp = (celsius_to_fahrenheit * userTemp) + 32;
-                    JOptionPane.showMessageDialog(
-                            null,
-                            String.format("The Temperature in Celsius is: %.2f C", showNewTemp),
-                            "Results",
-                            JOptionPane.INFORMATION_MESSAGE
-                    );
-                    //debuging
-                    //System.out.println("Hello No " + showNewTemp);
-                    break;
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Please enter correct charecters",
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE
-                    );
-                }
-
-            } else if (JOptionPane.CANCEL_OPTION == decisionOutput) {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Thank for using our Temperatures Converter.\n\nBye",
-                        "Closing Program",
-                        JOptionPane.INFORMATION_MESSAGE
-                );
-                break;
-            } else {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Error Occured Please try again.",
-                        "404",
-                        JOptionPane.ERROR);
-                break;
             }
         }
-
     }
 }
