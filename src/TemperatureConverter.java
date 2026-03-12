777a0c1ef3a527bdf8925a026603618382be6ba3
Frans Chokoe <fmoyagabo@gmail.com>
2026-03-12 21:28:50 +0200

    Going in it

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class TemperatureConverter {
+    public static void main(String[] args){
+        //declaring variables
+        final double celsius_to_fahrenheit = (double)9/5;
+        final double fahrenheit_to_celsius = (double)5/9;
+        boolean programRunner = true;
+
+        //initiallizing the while loop
+
+
+    }
 }
